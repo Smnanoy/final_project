00892feff8b7229c2286cd88180702006464ccf9
Smnanoy <nanoysteppenaverde@gmail.com>
2024-12-18 14:22:56 +0800

    cc9

--- Code_Challenges/code_challenge9.py
+++ Code_Challenges/code_challenge9.py
@@ -3,7 +3,7 @@ def code_challenge9():
         b = 7
         for j in range(b, 0, -1):
                 for k in range(b - j):
-                        print(" ", end="")
+                        print("*", end="")
                 
         for n in range(j):
                 print("*", end="")
