b6f3cff08da410334349d4b32d60de5b102b2638
Smnanoy <nanoysteppenaverde@gmail.com>
2024-12-18 14:10:28 +0800

    cc3

---
+++ Code_Challenges/code_challenge3.py
@@ -0,0 +1,36 @@
+def code_challenge3():
+    fullname = input("please input your full name:")
+    City = input("please input your city:")
+    Province = input("please input your province:")
+    Postal_code = input("please input the postal code of the province:")
+    Cellphonenumber = input("please input your cellphone number:")
+    Email = input("please input your email:")
+    Birthday = input("please input you birhday:")
+    BirthPlace = input("please input your birth place:")
+    Age = input("please input your age:")
+    CivilStatus = input("please input your civil status:")
+    Height = input("please input your height in ft:")
+    Weight = input("please input your weight in kg:")
+    Religion = input("please input your religion:")
+    Gender = input("please input your gender:")
+    Citezenship = input("please input your cintezenship:")
+    FatherName = input("please input your father's name:")
+    SiblingName = input("please input your sibling's name:")
+    print("My name is:" + fullname + 
+        "\nI'm from the city of:" + City + 
+        "\nI'm from the province of:" + Province + 
+        "\nThe postal code of:" + Province + ",is the:" + Postal_code + 
+        "\nMy cellphone number is:" + Cellphonenumber + 
+        "\nMy Email is:" + Email + 
+        "\nMy Birthday is:" + Birthday + 
+        "\nMy Birth Place:" + BirthPlace + 
+        "\nMy age is:" + Age + 
+        "\nMy civil status is:" + CivilStatus +
+        "\nMy height in Ft is:" + Height + 
+        "\nMy weight in kilogram is:" + Weight + 
+        "\nMy religion is:" + Religion + 
+        "\nMy gender:" +Gender + 
+        "\nMy citezenship is" + Citezenship + 
+        "\nMy Father's full name is:" + FatherName + 
+        "\nMy sibling's full name is:" + SiblingName + 
+        "\nThis is my BIONOTE") 
